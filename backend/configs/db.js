9a5f57478ec6ad4aba39c9edc811692970958ef9
Babli Kumari <96077669+bablikumari2@users.noreply.github.com>
2022-04-17 19:46:29 +0530

    Update db.js

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
   
-    return mongoose.connect("mongodb+srv://babli:babli12@cluster0.ftssi.mongodb.net/MANAGER");
+    return mongoose.connect("mongodb+srv://babli:babli12@cluster0.ftssi.mongodb.net/manager");
 }
