fb8b74daaa1615c1db0eef114cf1d8711375850a
Babli Kumari <96077669+bablikumari2@users.noreply.github.com>
2022-04-17 20:45:51 +0530

    Update Home.jsx

--- fornted/manager/src/components/Home.jsx
+++ fornted/manager/src/components/Home.jsx
@@ -4,7 +4,7 @@ import { useNavigate} from "react-router-dom";
 export const Home = () =>{
     const navigate = useNavigate();
     const handleClick = ()=>{
-    navigate("/login")
+    navigate("/flat")
     }
 
     return (<div id="homeMain">
